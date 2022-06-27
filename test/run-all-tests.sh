a005f34f0e2048c5dca1158a9a4934627bf8da2a
Slaven Falandys <sfalandys@yahoo.com>
2022-06-27 22:16:42 +0200

    Updated test script.

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -9,61 +9,18 @@ CXXFLAGS="-Wall -Wextra -Werror -Wfatal-errors -Wpedantic -std=c++11 -g"
 # Name of the output executable file:
 OUT_FILE="test.out"
 
-# -----------------------------------------------------------------------------
-# Testing test_allocator_XXX
-# -----------------------------------------------------------------------------
-
-# List of input files for testing:
-INPUT_FILES=(
-    "test_allocator_1.cpp"
-    "test_allocator_2.cpp"
-    "test_allocator_3.cpp"
-    "test_allocator_4.cpp"
-)
-
-for IN_FILE in ${INPUT_FILES[@]}; do
-
-    # Store build command in string:
-    BUILD_COMMAND="$CXX $CXXFLAGS -I ../include -o $OUT_FILE $IN_FILE"
-
-    # Execute build command:
-    $BUILD_COMMAND
-
-    if [ $? -ne 0 ]
-    then
-        echo ""
-        echo "ERROR: Build failed."
-        echo "> File: $IN_FILE"
-        echo "> Build command: $BUILD_COMMAND"
-        exit
-    fi
-
-    # Execute valgrind:
-    valgrind --leak-check=full ./$OUT_FILE
-
-    if [ $? -ne 0 ]
-    then
-        echo ""
-        echo "ERROR: Test failed."
-        echo "> File: $IN_FILE"
-        echo "> Build command: $BUILD_COMMAND"
-        exit
-    fi
-
-done
-
-# -----------------------------------------------------------------------------
-# Testing containers
-# -----------------------------------------------------------------------------
-
-# List of allocators:
-ALLOCATORS=(
+# List of allocators which will be used during testing:
+TEST_ALLOCATORS=(
     "std::allocator"
-    "sfl::test_allocator_1"
-    "sfl::test_allocator_2"
-    "sfl::test_allocator_3"
-    "sfl::test_allocator_4"
+    "sfl::stateless_alloc"
+    "sfl::stateless_alloc_no_prop"
+    "sfl::stateless_fancy_alloc"
+    "sfl::statefull_alloc"
 )
+
+# -----------------------------------------------------------------------------
+# Testing all small_XXX containers and compact_vector
+# -----------------------------------------------------------------------------
 
 # List of input files for testing:
 INPUT_FILES=(
@@ -81,10 +38,55 @@ INPUT_FILES=(
 
 for IN_FILE in ${INPUT_FILES[@]}; do
 
-    for ALLOC in ${ALLOCATORS[@]}; do
+    for ALLOC in ${TEST_ALLOCATORS[@]}; do
 
         # Store build command in string:
         BUILD_COMMAND="$CXX $CXXFLAGS -D SFL_TEST_ALLOCATOR=$ALLOC -I ../include -o $OUT_FILE $IN_FILE"
+
+        # Execute build command:
+        $BUILD_COMMAND
+
+        if [ $? -ne 0 ]
+        then
+            echo ""
+            echo "ERROR: Build failed."
+            echo "> File: $IN_FILE"
+            echo "> Allocator: $ALLOC"
+            echo "> Build command: $BUILD_COMMAND"
+            exit
+        fi
+
+        # Execute valgrind:
+        valgrind --leak-check=full ./$OUT_FILE
+
+        if [ $? -ne 0 ]
+        then
+            echo ""
+            echo "ERROR: Test failed."
+            echo "> File: $IN_FILE"
+            echo "> Allocator: $ALLOC"
+            echo "> Build command: $BUILD_COMMAND"
+            exit
+        fi
+
+    done
+
+done
+
+# -----------------------------------------------------------------------------
+# Testing segmented_vector
+# -----------------------------------------------------------------------------
+
+IN_FILE="test_segmented_vector.cpp"
+
+SEGMENT_SIZES=(1 2 4 100)
+
+for SEG_SZ in ${SEGMENT_SIZES[@]}; do
+
+    for ALLOC in ${TEST_ALLOCATORS[@]}; do
+
+        # Store build command in string:
+        BUILD_COMMAND="$CXX $CXXFLAGS -D SFL_TEST_ALLOCATOR=$ALLOC -I ../include -o $OUT_FILE $IN_FILE -D TP_N=$SEG_SZ"
 
         # Execute build command:
         $BUILD_COMMAND
