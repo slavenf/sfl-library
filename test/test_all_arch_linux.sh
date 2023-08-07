f5e95ad4abdb9b09b658691273d0115c05f90a1f
Slaven Falandys <sfalandys@yahoo.com>
2023-08-07 19:59:49 +0200

    Updated test script.

--- test/test_all_arch_linux.sh
+++ test/test_all_arch_linux.sh
@@ -6,6 +6,7 @@ CXX="g++ -std=c++11" ./test.sh all
 CXX="g++ -std=c++14" ./test.sh all
 CXX="g++ -std=c++17" ./test.sh all
 CXX="g++ -std=c++20" ./test.sh all
+CXX="g++ -std=c++23" ./test.sh all
 
 CXX="clang++ -std=c++11" ./test.sh all
 CXX="clang++ -std=c++14" ./test.sh all
