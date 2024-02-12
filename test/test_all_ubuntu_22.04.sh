a89b32772f2e062d96a36b52c8842a4ed2600bc7
Slaven Falandys <sfalandys@yahoo.com>
2024-02-12 20:12:02 +0100

    Test all

---
+++ test/test_all_ubuntu_22.04.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+run()
+{
+    # Print command
+    echo :::: Starting test \[$1\]
+
+    # Execute command
+    eval $1
+
+    if [ $? -ne 0 ] ; then
+        echo :::: ERROR. Test failed \[$1\]
+        echo :::: ALL STOP.
+        exit 1
+    fi
+
+    echo :::: Finished test \[$1\]
+}
+
+run "make CXX=\"g++     -std=c++11\" -j"
+run "make CXX=\"g++     -std=c++14\" -j"
+run "make CXX=\"g++     -std=c++17\" -j"
+run "make CXX=\"g++     -std=c++20\" -j"
+run "make CXX=\"g++     -std=c++23\" -j"
+run "make CXX=\"clang++ -std=c++11\" -j"
+run "make CXX=\"clang++ -std=c++14\" -j"
+run "make CXX=\"clang++ -std=c++17\" -j"
+run "make CXX=\"clang++ -std=c++20\" -j"
+
+echo :::: Finished all tests.
+echo :::: THE END.
