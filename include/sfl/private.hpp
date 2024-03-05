6570abca132b29c6615d15d3f6a56d45eabd9373
Slaven Falandys <sfalandys@yahoo.com>
2024-03-05 21:47:11 +0100

    Implemented hierarchical algorithm `uninitialized_default_construct_n`

--- include/sfl/private.hpp
+++ include/sfl/private.hpp
@@ -976,7 +976,8 @@ void uninitialized_default_construct(Allocator& a, ForwardIt first, ForwardIt la
     }
 }
 
-template <typename Allocator, typename ForwardIt, typename Size>
+template <typename Allocator, typename ForwardIt, typename Size,
+          sfl::dtl::enable_if_t< !sfl::dtl::is_segmented_iterator<ForwardIt>::value >* = nullptr>
 ForwardIt uninitialized_default_construct_n(Allocator& a, ForwardIt first, Size n)
 {
     ForwardIt curr = first;
@@ -993,6 +994,58 @@ ForwardIt uninitialized_default_construct_n(Allocator& a, ForwardIt first, Size 
     SFL_CATCH (...)
     {
         sfl::dtl::destroy(a, first, curr);
+        SFL_RETHROW;
+    }
+}
+
+template <typename Allocator, typename ForwardIt, typename Size,
+          sfl::dtl::enable_if_t< sfl::dtl::is_segmented_iterator<ForwardIt>::value >* = nullptr>
+ForwardIt uninitialized_default_construct_n(Allocator& a, ForwardIt first, Size n)
+{
+    using traits = sfl::dtl::segmented_iterator_traits<ForwardIt>;
+
+    auto curr_local = traits::local(first);
+    auto curr_seg   = traits::segment(first);
+
+    auto remainining = n;
+
+    SFL_TRY
+    {
+        while (true)
+        {
+            using difference_type =
+                typename std::iterator_traits<typename traits::local_iterator>::difference_type;
+
+            const auto count = std::min<difference_type>
+            (
+                remainining,
+                std::distance(curr_local, traits::end(curr_seg))
+            );
+
+            curr_local = sfl::dtl::uninitialized_default_construct_n
+            (
+                a,
+                curr_local,
+                count
+            );
+
+            remainining -= count;
+
+            SFL_ASSERT(remainining <= n && "Bug in algorithm. Please report it.");
+
+            if (remainining == 0)
+            {
+                return traits::compose(curr_seg, curr_local);
+            }
+
+            ++curr_seg;
+
+            curr_local = traits::begin(curr_seg);
+        }
+    }
+    SFL_CATCH (...)
+    {
+        sfl::dtl::destroy_n(a, first, n - remainining);
         SFL_RETHROW;
     }
 }
