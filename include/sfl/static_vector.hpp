37802b97f6d33de28607e98f08f0f702dbf4ecf5
Slaven Falandys <sfalandys@yahoo.com>
2024-03-24 23:48:04 +0100

    Renamed variable

--- include/sfl/static_vector.hpp
+++ include/sfl/static_vector.hpp
@@ -566,8 +566,7 @@ public:
         else
         {
             const pointer p2 = data_.last_ - 1;
-
-            const pointer old_last = data_.last_;
+            const pointer p3 = data_.last_;
 
             // The order of operations is critical. First we will construct
             // temporary value because arguments `args...` can contain
@@ -588,7 +587,7 @@ public:
             (
                 p1,
                 p2,
-                old_last
+                p3
             );
 
             *p1 = std::move(tmp);
