9717243fade9ca5a1563fcc80eb12e418b2737ac
Slaven Falandys <sfalandys@yahoo.com>
2024-04-03 21:35:45 +0200

    Added member function `insert`

--- test/static_flat_map.cpp
+++ test/static_flat_map.cpp
@@ -842,6 +842,231 @@ void test_static_flat_map()
             CHECK(map.nth(5)->first == 60); CHECK(map.nth(5)->second == 1);
         }
     }
+
+    PRINT("Test insert(const value_type&)");
+    {
+        sfl::static_flat_map<xint, xint, 100, std::less<xint>> map;
+
+        using value_type = std::pair<xint, xint>;
+
+        {
+            value_type value_20_1(20, 1);
+            value_type value_40_1(40, 1);
+            value_type value_60_1(60, 1);
+
+            value_type value_10_1(10, 1);
+            value_type value_30_1(30, 1);
+            value_type value_50_1(50, 1);
+
+            CHECK(map.insert(value_20_1) == std::make_pair(map.nth(0), true));
+            CHECK(map.insert(value_40_1) == std::make_pair(map.nth(1), true));
+            CHECK(map.insert(value_60_1) == std::make_pair(map.nth(2), true));
+
+            CHECK(map.insert(value_10_1) == std::make_pair(map.nth(0), true));
+            CHECK(map.insert(value_30_1) == std::make_pair(map.nth(2), true));
+            CHECK(map.insert(value_50_1) == std::make_pair(map.nth(4), true));
+
+            CHECK(map.size() == 6);
+            CHECK(map.nth(0)->first == 10); CHECK(map.nth(0)->second == 1);
+            CHECK(map.nth(1)->first == 20); CHECK(map.nth(1)->second == 1);
+            CHECK(map.nth(2)->first == 30); CHECK(map.nth(2)->second == 1);
+            CHECK(map.nth(3)->first == 40); CHECK(map.nth(3)->second == 1);
+            CHECK(map.nth(4)->first == 50); CHECK(map.nth(4)->second == 1);
+            CHECK(map.nth(5)->first == 60); CHECK(map.nth(5)->second == 1);
+
+            CHECK(value_20_1.first == 20); CHECK(value_20_1.second == 1);
+            CHECK(value_40_1.first == 40); CHECK(value_40_1.second == 1);
+            CHECK(value_60_1.first == 60); CHECK(value_60_1.second == 1);
+
+            CHECK(value_10_1.first == 10); CHECK(value_10_1.second == 1);
+            CHECK(value_30_1.first == 30); CHECK(value_30_1.second == 1);
+            CHECK(value_50_1.first == 50); CHECK(value_50_1.second == 1);
+        }
+
+        {
+            value_type value_20_2(20, 2);
+            value_type value_40_2(40, 2);
+            value_type value_60_2(60, 2);
+
+            value_type value_10_2(10, 2);
+            value_type value_30_2(30, 2);
+            value_type value_50_2(50, 2);
+
+            CHECK(map.insert(value_20_2) == std::make_pair(map.nth(1), false));
+            CHECK(map.insert(value_40_2) == std::make_pair(map.nth(3), false));
+            CHECK(map.insert(value_60_2) == std::make_pair(map.nth(5), false));
+
+            CHECK(map.insert(value_10_2) == std::make_pair(map.nth(0), false));
+            CHECK(map.insert(value_30_2) == std::make_pair(map.nth(2), false));
+            CHECK(map.insert(value_50_2) == std::make_pair(map.nth(4), false));
+
+            CHECK(map.size() == 6);
+            CHECK(map.nth(0)->first == 10); CHECK(map.nth(0)->second == 1);
+            CHECK(map.nth(1)->first == 20); CHECK(map.nth(1)->second == 1);
+            CHECK(map.nth(2)->first == 30); CHECK(map.nth(2)->second == 1);
+            CHECK(map.nth(3)->first == 40); CHECK(map.nth(3)->second == 1);
+            CHECK(map.nth(4)->first == 50); CHECK(map.nth(4)->second == 1);
+            CHECK(map.nth(5)->first == 60); CHECK(map.nth(5)->second == 1);
+
+            CHECK(value_20_2.first == 20); CHECK(value_20_2.second == 2);
+            CHECK(value_40_2.first == 40); CHECK(value_40_2.second == 2);
+            CHECK(value_60_2.first == 60); CHECK(value_60_2.second == 2);
+
+            CHECK(value_10_2.first == 10); CHECK(value_10_2.second == 2);
+            CHECK(value_30_2.first == 30); CHECK(value_30_2.second == 2);
+            CHECK(value_50_2.first == 50); CHECK(value_50_2.second == 2);
+        }
+    }
+
+    PRINT("Test insert(value_type&&)");
+    {
+        sfl::static_flat_map<xint, xint, 100, std::less<xint>> map;
+
+        using value_type = std::pair<xint, xint>;
+
+        {
+            value_type value_20_1(20, 1);
+            value_type value_40_1(40, 1);
+            value_type value_60_1(60, 1);
+
+            value_type value_10_1(10, 1);
+            value_type value_30_1(30, 1);
+            value_type value_50_1(50, 1);
+
+            CHECK(map.insert(std::move(value_20_1)) == std::make_pair(map.nth(0), true));
+            CHECK(map.insert(std::move(value_40_1)) == std::make_pair(map.nth(1), true));
+            CHECK(map.insert(std::move(value_60_1)) == std::make_pair(map.nth(2), true));
+
+            CHECK(map.insert(std::move(value_10_1)) == std::make_pair(map.nth(0), true));
+            CHECK(map.insert(std::move(value_30_1)) == std::make_pair(map.nth(2), true));
+            CHECK(map.insert(std::move(value_50_1)) == std::make_pair(map.nth(4), true));
+
+            CHECK(map.size() == 6);
+            CHECK(map.nth(0)->first == 10); CHECK(map.nth(0)->second == 1);
+            CHECK(map.nth(1)->first == 20); CHECK(map.nth(1)->second == 1);
+            CHECK(map.nth(2)->first == 30); CHECK(map.nth(2)->second == 1);
+            CHECK(map.nth(3)->first == 40); CHECK(map.nth(3)->second == 1);
+            CHECK(map.nth(4)->first == 50); CHECK(map.nth(4)->second == 1);
+            CHECK(map.nth(5)->first == 60); CHECK(map.nth(5)->second == 1);
+
+            CHECK(value_20_1.first == -20); CHECK(value_20_1.second == -1);
+            CHECK(value_40_1.first == -40); CHECK(value_40_1.second == -1);
+            CHECK(value_60_1.first == -60); CHECK(value_60_1.second == -1);
+
+            CHECK(value_10_1.first == -10); CHECK(value_10_1.second == -1);
+            CHECK(value_30_1.first == -30); CHECK(value_30_1.second == -1);
+            CHECK(value_50_1.first == -50); CHECK(value_50_1.second == -1);
+        }
+
+        {
+            value_type value_20_2(20, 2);
+            value_type value_40_2(40, 2);
+            value_type value_60_2(60, 2);
+
+            value_type value_10_2(10, 2);
+            value_type value_30_2(30, 2);
+            value_type value_50_2(50, 2);
+
+            CHECK(map.insert(std::move(value_20_2)) == std::make_pair(map.nth(1), false));
+            CHECK(map.insert(std::move(value_40_2)) == std::make_pair(map.nth(3), false));
+            CHECK(map.insert(std::move(value_60_2)) == std::make_pair(map.nth(5), false));
+
+            CHECK(map.insert(std::move(value_10_2)) == std::make_pair(map.nth(0), false));
+            CHECK(map.insert(std::move(value_30_2)) == std::make_pair(map.nth(2), false));
+            CHECK(map.insert(std::move(value_50_2)) == std::make_pair(map.nth(4), false));
+
+            CHECK(map.size() == 6);
+            CHECK(map.nth(0)->first == 10); CHECK(map.nth(0)->second == 1);
+            CHECK(map.nth(1)->first == 20); CHECK(map.nth(1)->second == 1);
+            CHECK(map.nth(2)->first == 30); CHECK(map.nth(2)->second == 1);
+            CHECK(map.nth(3)->first == 40); CHECK(map.nth(3)->second == 1);
+            CHECK(map.nth(4)->first == 50); CHECK(map.nth(4)->second == 1);
+            CHECK(map.nth(5)->first == 60); CHECK(map.nth(5)->second == 1);
+
+            CHECK(value_20_2.first == +20); CHECK(value_20_2.second == +2);
+            CHECK(value_40_2.first == +40); CHECK(value_40_2.second == +2);
+            CHECK(value_60_2.first == +60); CHECK(value_60_2.second == +2);
+
+            CHECK(value_10_2.first == +10); CHECK(value_10_2.second == +2);
+            CHECK(value_30_2.first == +30); CHECK(value_30_2.second == +2);
+            CHECK(value_50_2.first == +50); CHECK(value_50_2.second == +2);
+        }
+    }
+
+    PRINT("Test insert(P&&)");
+    {
+        sfl::static_flat_map<xint, xint, 100, std::less<xint>> map;
+
+        using value_type = std::pair<long, long>;
+
+        {
+            value_type value_20_1(20, 1);
+            value_type value_40_1(40, 1);
+            value_type value_60_1(60, 1);
+
+            value_type value_10_1(10, 1);
+            value_type value_30_1(30, 1);
+            value_type value_50_1(50, 1);
+
+            CHECK(map.insert(value_20_1) == std::make_pair(map.nth(0), true));
+            CHECK(map.insert(value_40_1) == std::make_pair(map.nth(1), true));
+            CHECK(map.insert(value_60_1) == std::make_pair(map.nth(2), true));
+
+            CHECK(map.insert(value_10_1) == std::make_pair(map.nth(0), true));
+            CHECK(map.insert(value_30_1) == std::make_pair(map.nth(2), true));
+            CHECK(map.insert(value_50_1) == std::make_pair(map.nth(4), true));
+
+            CHECK(map.size() == 6);
+            CHECK(map.nth(0)->first == 10); CHECK(map.nth(0)->second == 1);
+            CHECK(map.nth(1)->first == 20); CHECK(map.nth(1)->second == 1);
+            CHECK(map.nth(2)->first == 30); CHECK(map.nth(2)->second == 1);
+            CHECK(map.nth(3)->first == 40); CHECK(map.nth(3)->second == 1);
+            CHECK(map.nth(4)->first == 50); CHECK(map.nth(4)->second == 1);
+            CHECK(map.nth(5)->first == 60); CHECK(map.nth(5)->second == 1);
+
+            CHECK(value_20_1.first == 20); CHECK(value_20_1.second == 1);
+            CHECK(value_40_1.first == 40); CHECK(value_40_1.second == 1);
+            CHECK(value_60_1.first == 60); CHECK(value_60_1.second == 1);
+
+            CHECK(value_10_1.first == 10); CHECK(value_10_1.second == 1);
+            CHECK(value_30_1.first == 30); CHECK(value_30_1.second == 1);
+            CHECK(value_50_1.first == 50); CHECK(value_50_1.second == 1);
+        }
+
+        {
+            value_type value_20_2(20, 2);
+            value_type value_40_2(40, 2);
+            value_type value_60_2(60, 2);
+
+            value_type value_10_2(10, 2);
+            value_type value_30_2(30, 2);
+            value_type value_50_2(50, 2);
+
+            CHECK(map.insert(value_20_2) == std::make_pair(map.nth(1), false));
+            CHECK(map.insert(value_40_2) == std::make_pair(map.nth(3), false));
+            CHECK(map.insert(value_60_2) == std::make_pair(map.nth(5), false));
+
+            CHECK(map.insert(value_10_2) == std::make_pair(map.nth(0), false));
+            CHECK(map.insert(value_30_2) == std::make_pair(map.nth(2), false));
+            CHECK(map.insert(value_50_2) == std::make_pair(map.nth(4), false));
+
+            CHECK(map.size() == 6);
+            CHECK(map.nth(0)->first == 10); CHECK(map.nth(0)->second == 1);
+            CHECK(map.nth(1)->first == 20); CHECK(map.nth(1)->second == 1);
+            CHECK(map.nth(2)->first == 30); CHECK(map.nth(2)->second == 1);
+            CHECK(map.nth(3)->first == 40); CHECK(map.nth(3)->second == 1);
+            CHECK(map.nth(4)->first == 50); CHECK(map.nth(4)->second == 1);
+            CHECK(map.nth(5)->first == 60); CHECK(map.nth(5)->second == 1);
+
+            CHECK(value_20_2.first == 20); CHECK(value_20_2.second == 2);
+            CHECK(value_40_2.first == 40); CHECK(value_40_2.second == 2);
+            CHECK(value_60_2.first == 60); CHECK(value_60_2.second == 2);
+
+            CHECK(value_10_2.first == 10); CHECK(value_10_2.second == 2);
+            CHECK(value_30_2.first == 30); CHECK(value_30_2.second == 2);
+            CHECK(value_50_2.first == 50); CHECK(value_50_2.second == 2);
+        }
+    }
 }
 
 int main()
