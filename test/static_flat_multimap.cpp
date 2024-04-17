25436631426c50d9fafbebd8ad2d7bbe1bba0afb
Slaven Falandys <sfalandys@yahoo.com>
2024-04-17 22:35:23 +0200

    Added constructor (iterator range)

--- test/static_flat_multimap.cpp
+++ test/static_flat_multimap.cpp
@@ -2179,6 +2179,88 @@ void test_static_flat_multimap()
         CHECK(map.capacity() == 100);
         CHECK(map.available() == 100);
     }
+
+    PRINT("Test container(InputIt, InputIt)");
+    {
+        std::vector<std::pair<xint, xint>> data
+        (
+            {
+                {20, 1},
+                {40, 1},
+                {60, 1},
+
+                {10, 1},
+                {30, 1},
+                {50, 1},
+
+                {20, 2},
+                {40, 2},
+                {60, 2},
+
+                {10, 2},
+                {30, 2},
+                {50, 2}
+            }
+        );
+
+        sfl::static_flat_multimap<xint, xint, 100, std::less<xint>> map(data.begin(), data.end());
+
+        CHECK(map.size() == 12);
+        CHECK(map.nth( 0)->first == 10); CHECK(map.nth( 0)->second == 2);
+        CHECK(map.nth( 1)->first == 10); CHECK(map.nth( 1)->second == 1);
+        CHECK(map.nth( 2)->first == 20); CHECK(map.nth( 2)->second == 2);
+        CHECK(map.nth( 3)->first == 20); CHECK(map.nth( 3)->second == 1);
+        CHECK(map.nth( 4)->first == 30); CHECK(map.nth( 4)->second == 2);
+        CHECK(map.nth( 5)->first == 30); CHECK(map.nth( 5)->second == 1);
+        CHECK(map.nth( 6)->first == 40); CHECK(map.nth( 6)->second == 2);
+        CHECK(map.nth( 7)->first == 40); CHECK(map.nth( 7)->second == 1);
+        CHECK(map.nth( 8)->first == 50); CHECK(map.nth( 8)->second == 2);
+        CHECK(map.nth( 9)->first == 50); CHECK(map.nth( 9)->second == 1);
+        CHECK(map.nth(10)->first == 60); CHECK(map.nth(10)->second == 2);
+        CHECK(map.nth(11)->first == 60); CHECK(map.nth(11)->second == 1);
+    }
+
+    PRINT("Test container(InputIt, InputIt, const Compare&)");
+    {
+        std::vector<std::pair<xint, xint>> data
+        (
+            {
+                {20, 1},
+                {40, 1},
+                {60, 1},
+
+                {10, 1},
+                {30, 1},
+                {50, 1},
+
+                {20, 2},
+                {40, 2},
+                {60, 2},
+
+                {10, 2},
+                {30, 2},
+                {50, 2}
+            }
+        );
+
+        std::less<xint> comp;
+
+        sfl::static_flat_multimap<xint, xint, 100, std::less<xint>> map(data.begin(), data.end(), comp);
+
+        CHECK(map.size() == 12);
+        CHECK(map.nth( 0)->first == 10); CHECK(map.nth( 0)->second == 2);
+        CHECK(map.nth( 1)->first == 10); CHECK(map.nth( 1)->second == 1);
+        CHECK(map.nth( 2)->first == 20); CHECK(map.nth( 2)->second == 2);
+        CHECK(map.nth( 3)->first == 20); CHECK(map.nth( 3)->second == 1);
+        CHECK(map.nth( 4)->first == 30); CHECK(map.nth( 4)->second == 2);
+        CHECK(map.nth( 5)->first == 30); CHECK(map.nth( 5)->second == 1);
+        CHECK(map.nth( 6)->first == 40); CHECK(map.nth( 6)->second == 2);
+        CHECK(map.nth( 7)->first == 40); CHECK(map.nth( 7)->second == 1);
+        CHECK(map.nth( 8)->first == 50); CHECK(map.nth( 8)->second == 2);
+        CHECK(map.nth( 9)->first == 50); CHECK(map.nth( 9)->second == 1);
+        CHECK(map.nth(10)->first == 60); CHECK(map.nth(10)->second == 2);
+        CHECK(map.nth(11)->first == 60); CHECK(map.nth(11)->second == 1);
+    }
 }
 
 int main()
