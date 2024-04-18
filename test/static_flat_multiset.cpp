e1a809d842923e05d90f7c8ec917c1eb06514baf
Slaven Falandys <sfalandys@yahoo.com>
2024-04-18 22:11:34 +0200

    Added constructor (iterator range)

--- test/static_flat_multiset.cpp
+++ test/static_flat_multiset.cpp
@@ -2019,6 +2019,88 @@ void test_static_flat_multiset()
         CHECK(set.capacity() == 100);
         CHECK(set.available() == 100);
     }
+
+    PRINT("Test container(InputIt, InputIt)");
+    {
+        std::vector<xint_xint> data
+        (
+            {
+                {20, 1},
+                {40, 1},
+                {60, 1},
+
+                {10, 1},
+                {30, 1},
+                {50, 1},
+
+                {20, 2},
+                {40, 2},
+                {60, 2},
+
+                {10, 2},
+                {30, 2},
+                {50, 2}
+            }
+        );
+
+        sfl::static_flat_multiset<xint_xint, 100, std::less<xint_xint>> set(data.begin(), data.end());
+
+        CHECK(set.size() == 12);
+        CHECK(set.nth( 0)->first == 10); CHECK(set.nth( 0)->second == 2);
+        CHECK(set.nth( 1)->first == 10); CHECK(set.nth( 1)->second == 1);
+        CHECK(set.nth( 2)->first == 20); CHECK(set.nth( 2)->second == 2);
+        CHECK(set.nth( 3)->first == 20); CHECK(set.nth( 3)->second == 1);
+        CHECK(set.nth( 4)->first == 30); CHECK(set.nth( 4)->second == 2);
+        CHECK(set.nth( 5)->first == 30); CHECK(set.nth( 5)->second == 1);
+        CHECK(set.nth( 6)->first == 40); CHECK(set.nth( 6)->second == 2);
+        CHECK(set.nth( 7)->first == 40); CHECK(set.nth( 7)->second == 1);
+        CHECK(set.nth( 8)->first == 50); CHECK(set.nth( 8)->second == 2);
+        CHECK(set.nth( 9)->first == 50); CHECK(set.nth( 9)->second == 1);
+        CHECK(set.nth(10)->first == 60); CHECK(set.nth(10)->second == 2);
+        CHECK(set.nth(11)->first == 60); CHECK(set.nth(11)->second == 1);
+    }
+
+    PRINT("Test container(InputIt, InputIt, const Compare&)");
+    {
+        std::vector<xint_xint> data
+        (
+            {
+                {20, 1},
+                {40, 1},
+                {60, 1},
+
+                {10, 1},
+                {30, 1},
+                {50, 1},
+
+                {20, 2},
+                {40, 2},
+                {60, 2},
+
+                {10, 2},
+                {30, 2},
+                {50, 2}
+            }
+        );
+
+        std::less<xint_xint> comp;
+
+        sfl::static_flat_multiset<xint_xint, 100, std::less<xint_xint>> set(data.begin(), data.end(), comp);
+
+        CHECK(set.size() == 12);
+        CHECK(set.nth( 0)->first == 10); CHECK(set.nth( 0)->second == 2);
+        CHECK(set.nth( 1)->first == 10); CHECK(set.nth( 1)->second == 1);
+        CHECK(set.nth( 2)->first == 20); CHECK(set.nth( 2)->second == 2);
+        CHECK(set.nth( 3)->first == 20); CHECK(set.nth( 3)->second == 1);
+        CHECK(set.nth( 4)->first == 30); CHECK(set.nth( 4)->second == 2);
+        CHECK(set.nth( 5)->first == 30); CHECK(set.nth( 5)->second == 1);
+        CHECK(set.nth( 6)->first == 40); CHECK(set.nth( 6)->second == 2);
+        CHECK(set.nth( 7)->first == 40); CHECK(set.nth( 7)->second == 1);
+        CHECK(set.nth( 8)->first == 50); CHECK(set.nth( 8)->second == 2);
+        CHECK(set.nth( 9)->first == 50); CHECK(set.nth( 9)->second == 1);
+        CHECK(set.nth(10)->first == 60); CHECK(set.nth(10)->second == 2);
+        CHECK(set.nth(11)->first == 60); CHECK(set.nth(11)->second == 1);
+    }
 }
 
 int main()
