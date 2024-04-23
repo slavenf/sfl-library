29c53bc236f66e80b7036ba66b603c77c55f95f9
Slaven Falandys <sfalandys@yahoo.com>
2024-04-23 21:05:14 +0200

    Added member function `at`

--- include/sfl/static_unordered_flat_map.hpp
+++ include/sfl/static_unordered_flat_map.hpp
@@ -717,6 +717,32 @@ public:
     // ---- ELEMENT ACCESS ----------------------------------------------------
     //
 
+    SFL_NODISCARD
+    T& at(const Key& key)
+    {
+        auto it = find(key);
+
+        if (it == end())
+        {
+            sfl::dtl::throw_out_of_range("sfl::static_unordered_flat_map::at");
+        }
+
+        return it->second;
+    }
+
+    SFL_NODISCARD
+    const T& at(const Key& key) const
+    {
+        auto it = find(key);
+
+        if (it == end())
+        {
+            sfl::dtl::throw_out_of_range("sfl::static_unordered_flat_map::at");
+        }
+
+        return it->second;
+    }
+
 private:
 
     template <typename... Args>
