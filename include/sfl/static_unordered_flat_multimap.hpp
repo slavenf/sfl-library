0cdd7d5008484c2d6e0d09be135c992f835bfcc5
Slaven Falandys <sfalandys@yahoo.com>
2024-04-26 15:24:31 +0200

    Added member function `erase`

--- include/sfl/static_unordered_flat_multimap.hpp
+++ include/sfl/static_unordered_flat_multimap.hpp
@@ -407,6 +407,31 @@ public:
         insert(ilist.begin(), ilist.end());
     }
 
+    iterator erase(iterator pos)
+    {
+        return erase(const_iterator(pos));
+    }
+
+    iterator erase(const_iterator pos)
+    {
+        SFL_ASSERT(cbegin() <= pos && pos < cend());
+
+        const difference_type offset = std::distance(cbegin(), pos);
+
+        const pointer p = data_.first_ + offset;
+
+        if (p < data_.last_ - 1)
+        {
+            *p = std::move(*(data_.last_ - 1));
+        }
+
+        --data_.last_;
+
+        sfl::dtl::destroy_at(data_.last_);
+
+        return p;
+    }
+
     //
     // ---- LOOKUP ------------------------------------------------------------
     //
