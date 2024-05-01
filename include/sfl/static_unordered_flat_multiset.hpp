e708e1519812a40cc25e03025d8e39105d027148
Slaven Falandys <sfalandys@yahoo.com>
2024-05-01 12:29:27 +0200

    Added move constructor

--- include/sfl/static_unordered_flat_multiset.hpp
+++ include/sfl/static_unordered_flat_multiset.hpp
@@ -193,6 +193,17 @@ public:
         );
     }
 
+    static_unordered_flat_multiset(static_unordered_flat_multiset&& other)
+        : data_(std::move(other.data_.ref_to_equal()))
+    {
+        data_.last_ = sfl::dtl::uninitialized_move
+        (
+            std::make_move_iterator(pointer(other.data_.first_)),
+            std::make_move_iterator(pointer(other.data_.last_)),
+            data_.first_
+        );
+    }
+
     ~static_unordered_flat_multiset()
     {
         sfl::dtl::destroy(data_.first_, data_.last_);
