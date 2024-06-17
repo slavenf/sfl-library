ed837a1b15c61cf05ef59435d55a5b4dc6784de9
Slaven Falandys <sfalandys@yahoo.com>
2024-06-17 12:21:54 +0200

    Compile with GCC 7 and Clang 5 on CentOS 7

--- test/test_all_centos_7.sh
+++ test/test_all_centos_7.sh
@@ -23,5 +23,25 @@ run "make CXX=\"clang++ -std=c++11\" SILENT=1 -j$(nproc)"
 run "make CXX=\"g++     -std=c++11 -fno-exceptions -DSFL_NO_EXCEPTIONS\" SILENT=1 -j$(nproc)"
 run "make CXX=\"clang++ -std=c++11 -fno-exceptions -DSFL_NO_EXCEPTIONS\" SILENT=1 -j$(nproc)"
 
+if command -v /opt/rh/devtoolset-7/root/bin/g++ > /dev/null 2>&1; then
+
+run "make CXX=\"/opt/rh/devtoolset-7/root/bin/g++ -std=c++11\" SILENT=1 -j$(nproc)"
+run "make CXX=\"/opt/rh/devtoolset-7/root/bin/g++ -std=c++14\" SILENT=1 -j$(nproc)"
+
+run "make CXX=\"/opt/rh/devtoolset-7/root/bin/g++ -std=c++11 -fno-exceptions -DSFL_NO_EXCEPTIONS\" SILENT=1 -j$(nproc)"
+run "make CXX=\"/opt/rh/devtoolset-7/root/bin/g++ -std=c++14 -fno-exceptions -DSFL_NO_EXCEPTIONS\" SILENT=1 -j$(nproc)"
+
+fi
+
+if command -v /opt/rh/llvm-toolset-7/root/bin/clang++ > /dev/null 2>&1; then
+
+run "make CXX=\"/opt/rh/llvm-toolset-7/root/bin/clang++ -std=c++11\" SILENT=1 -j$(nproc)"
+run "make CXX=\"/opt/rh/llvm-toolset-7/root/bin/clang++ -std=c++14\" SILENT=1 -j$(nproc)"
+
+run "make CXX=\"/opt/rh/llvm-toolset-7/root/bin/clang++ -std=c++11 -fno-exceptions -DSFL_NO_EXCEPTIONS\" SILENT=1 -j$(nproc)"
+run "make CXX=\"/opt/rh/llvm-toolset-7/root/bin/clang++ -std=c++14 -fno-exceptions -DSFL_NO_EXCEPTIONS\" SILENT=1 -j$(nproc)"
+
+fi
+
 echo :::: Finished all tests.
 echo :::: THE END.
