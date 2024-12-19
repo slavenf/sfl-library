7e99788f026cf59719674e5245325028c99abad4
Slaven Falandys <sfalandys@yahoo.com>
2024-12-19 16:14:46 +0100

    Moved `segmented_iterator` to separate file

--- include/sfl/segmented_devector.hpp
+++ include/sfl/segmented_devector.hpp
@@ -24,6 +24,7 @@
 #include <sfl/private.hpp>
 #include <sfl/detail/concepts.hpp>
 #include <sfl/detail/cpp.hpp>
+#include <sfl/detail/segmented_iterator.hpp>
 #include <sfl/detail/tags.hpp>
 #include <sfl/detail/type_traits.hpp>
 
