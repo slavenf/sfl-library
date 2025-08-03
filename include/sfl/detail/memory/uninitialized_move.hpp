a52ab2733c2fb68cd37d6b60754cdc00917349e4
Slaven Falandys <sfalandys@yahoo.com>
2025-08-03 15:39:10 +0200

    Moved function `uninitialized_move` to separate file

--- include/sfl/detail/memory/uninitialized_move.hpp
+++ include/sfl/detail/memory/uninitialized_move.hpp
@@ -18,29 +18,15 @@
 // 3. This notice may not be removed or altered from any source distribution.
 //
 
-#ifndef SFL_DETAIL_UNINITIALIZED_MEMORY_ALGORITHMS_HPP_INCLUDED
-#define SFL_DETAIL_UNINITIALIZED_MEMORY_ALGORITHMS_HPP_INCLUDED
+#ifndef SFL_DETAIL_UNINITIALIZED_MOVE_HPP_INCLUDED
+#define SFL_DETAIL_UNINITIALIZED_MOVE_HPP_INCLUDED
 
 #include <sfl/detail/memory/construct_at.hpp>
-#include <sfl/detail/memory/construct_at_a.hpp>
-#include <sfl/detail/memory/default_construct_at.hpp>
 #include <sfl/detail/memory/destroy.hpp>
-#include <sfl/detail/memory/destroy_a.hpp>
-#include <sfl/detail/memory/destroy_at.hpp>
-#include <sfl/detail/memory/destroy_at_a.hpp>
-#include <sfl/detail/memory/destroy_n_a.hpp>
-#include <sfl/detail/memory/value_construct_at.hpp>
-#include <sfl/detail/type_traits/enable_if_t.hpp>
-#include <sfl/detail/type_traits/is_random_access_iterator.hpp>
-#include <sfl/detail/type_traits/is_segmented_iterator.hpp>
-#include <sfl/detail/type_traits/segmented_iterator_traits.hpp>
 #include <sfl/detail/cpp.hpp>
-#include <sfl/detail/to_address.hpp>
 
-#include <algorithm>
-#include <iterator>
-#include <memory>
-#include <utility>
+#include <memory>  // addressof
+#include <utility> // move
 
 namespace sfl
 {
@@ -48,8 +34,29 @@ namespace sfl
 namespace dtl
 {
 
+template <typename InputIt, typename ForwardIt>
+ForwardIt uninitialized_move(InputIt first, InputIt last, ForwardIt d_first)
+{
+    ForwardIt d_curr = d_first;
+    SFL_TRY
+    {
+        while (first != last)
+        {
+            sfl::dtl::construct_at(std::addressof(*d_curr), std::move(*first));
+            ++d_curr;
+            ++first;
+        }
+        return d_curr;
+    }
+    SFL_CATCH (...)
+    {
+        sfl::dtl::destroy(d_first, d_curr);
+        SFL_RETHROW;
+    }
+}
+
 } // namespace dtl
 
 } // namespace sfl
 
-#endif // SFL_DETAIL_UNINITIALIZED_MEMORY_ALGORITHMS_HPP_INCLUDED
+#endif // SFL_DETAIL_UNINITIALIZED_MOVE_HPP_INCLUDED
