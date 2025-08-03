22147567868675192e9067f4d17381fbc8a1af92
Slaven Falandys <sfalandys@yahoo.com>
2025-08-03 15:11:29 +0200

    Moved function `value_construct_at` to separate file

--- include/sfl/detail/uninitialized_memory_algorithms.hpp
+++ include/sfl/detail/uninitialized_memory_algorithms.hpp
@@ -26,6 +26,7 @@
 #include <sfl/detail/memory/destroy_a.hpp>
 #include <sfl/detail/memory/destroy_at_a.hpp>
 #include <sfl/detail/memory/destroy_n_a.hpp>
+#include <sfl/detail/memory/value_construct_at.hpp>
 #include <sfl/detail/type_traits/enable_if_t.hpp>
 #include <sfl/detail/type_traits/is_random_access_iterator.hpp>
 #include <sfl/detail/type_traits/is_segmented_iterator.hpp>
@@ -43,12 +44,6 @@ namespace sfl
 
 namespace dtl
 {
-
-template <typename T>
-void value_construct_at(T* p)
-{
-    ::new (static_cast<void*>(p)) T();
-}
 
 template <typename T, typename... Args>
 void construct_at(T* p, Args&&... args)
