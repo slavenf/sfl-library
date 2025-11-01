3afbec7ddae01a548f7ccf74339775f054d4c993
Slaven Falandys <sfalandys@yahoo.com>
2025-11-01 21:19:37 +0100

    Updated tests for `devector`

--- test/src/devector/devector.cpp
+++ test/src/devector/devector.cpp
@@ -18,55 +18,71 @@
 #include <sstream>
 #include <vector>
 
-// template <>
-// void test_devector<1>()
-// {
-//     #undef   TPARAM_ALLOCATOR
-//     #define  TPARAM_ALLOCATOR std::allocator
-//     #include "devector.inc"
-// }
+#define BEGIN_TEST(x) PRINT(x)
+#define END_TEST()
 
-// template <>
-// void test_devector<2>()
-// {
-//     #undef   TPARAM_ALLOCATOR
-//     #define  TPARAM_ALLOCATOR sfl::test::statefull_alloc
-//     #include "devector.inc"
-// }
+template <>
+void test_devector<1>()
+{
+    #undef   TPARAM_ALLOCATOR
+    #define  TPARAM_ALLOCATOR std::allocator
+    #include "devector.inc"
+}
 
-// template <>
-// void test_devector<3>()
-// {
-//     #undef   TPARAM_ALLOCATOR
-//     #define  TPARAM_ALLOCATOR sfl::test::stateless_alloc
-//     #include "devector.inc"
-// }
+template <>
+void test_devector<2>()
+{
+    #undef   TPARAM_ALLOCATOR
+    #define  TPARAM_ALLOCATOR sfl::test::statefull_alloc
+    #include "devector.inc"
+}
 
-// template <>
-// void test_devector<4>()
-// {
-//     #undef   TPARAM_ALLOCATOR
-//     #define  TPARAM_ALLOCATOR sfl::test::stateless_alloc_no_prop
-//     #include "devector.inc"
-// }
+template <>
+void test_devector<3>()
+{
+    #undef   TPARAM_ALLOCATOR
+    #define  TPARAM_ALLOCATOR sfl::test::stateless_alloc
+    #include "devector.inc"
+}
 
-// template <>
-// void test_devector<5>()
-// {
-//     #undef   TPARAM_ALLOCATOR
-//     #define  TPARAM_ALLOCATOR sfl::test::stateless_fancy_alloc
-//     #include "devector.inc"
-// }
+template <>
+void test_devector<4>()
+{
+    #undef   TPARAM_ALLOCATOR
+    #define  TPARAM_ALLOCATOR sfl::test::stateless_alloc_no_prop
+    #include "devector.inc"
+}
+
+template <>
+void test_devector<5>()
+{
+    #undef   TPARAM_ALLOCATOR
+    #define  TPARAM_ALLOCATOR sfl::test::stateless_fancy_alloc
+    #include "devector.inc"
+}
 
 int main()
 {
-    // test_devector<1>();
-    // test_devector<2>();
-    // test_devector<3>();
-    // test_devector<4>();
-    // test_devector<5>();
+    test_devector<1>();
+    test_devector<2>();
+    test_devector<3>();
+    test_devector<4>();
+    test_devector<5>();
 }
 
 #if SFL_CPP_VERSION >= SFL_CPP_20
-#include "test_cpp20_constexpr.inc"
-#endif
+
+#undef  BEGIN_TEST
+#undef  END_TEST
+
+#define BEGIN_TEST(x) static_assert([](){
+#define END_TEST()    return true;}());
+
+#undef  TPARAM_ALLOCATOR
+#define TPARAM_ALLOCATOR std::allocator
+
+#define TEST_CPP20_CONSTEXPR
+
+#include "devector.inc"
+
+#endif // SFL_CPP_VERSION >= SFL_CPP_20
