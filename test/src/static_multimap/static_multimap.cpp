b15e5ddd5522e24778a91c197efd51bc61991d5c
Slaven Falandys <sfalandys@yahoo.com>
2025-12-20 17:09:43 +0100

    Updated constexpr tests

--- test/src/static_multimap/static_multimap.cpp
+++ test/src/static_multimap/static_multimap.cpp
@@ -39,4 +39,57 @@ int main()
 
 #include "static_multimap.inc"
 
+///////////////////////////////////////////////////////////////////////////////
+
+namespace constexpr_test
+{
+
+using map_type = sfl::static_multimap<int, int, 8>;
+
+constexpr map_type create_map()
+{
+    map_type map;
+    map.emplace(10, 10);
+    map.emplace(20, 20);
+    map.emplace(30, 30);
+    return map;
+}
+
+constexpr bool test()
+{
+    map_type map1 = create_map();
+
+    map_type map2;
+
+    int sum_before = 0;
+
+    for (const auto& elem : map2)
+    {
+        sum_before += elem.first;
+    }
+
+    map2 = map1;
+
+    int sum_after = 0;
+
+    for (const auto& elem : map2)
+    {
+        sum_after += elem.first;
+    }
+
+    return sum_before == 0 && sum_after == 60;
+}
+
+static_assert(test());
+
+#if !(defined(__GNUC__) && !defined(__clang__))
+
+constexpr map_type g_map = create_map();
+
+static_assert(g_map.size() == 3);
+
+#endif
+
+} // namespace constexpr_test
+
 #endif // SFL_CPP_VERSION >= SFL_CPP_20
